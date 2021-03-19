30548601bafc3b3e2413afb66aede5fec8b3dfcc
zoranyankov <zoran_yankov@yahoo.com>
2021-03-20 01:20:57 +0200

    quard the profile page

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,6 +1,13 @@
+import { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
 
+import AppContext from '../AppContext';
 
 const Profile = (props) => {
+    const context = useContext(AppContext);
+    if (!context.isAuthName) {
+        return <Redirect to="/" />;
+    }
     return (
         <div>
             <h1>THE PROFILE PAGE</h1>
