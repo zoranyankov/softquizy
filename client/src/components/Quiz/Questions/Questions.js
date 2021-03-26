b408554975a43d0423f38e7d4eb3d44dd48a4cb5
zoranyankov <zoran_yankov@yahoo.com>
2021-03-26 12:40:58 +0200

    refactured quiz and record results in BE
    get questions in quiz component and use local states in questions component and record results in BE

--- client/src/components/Quiz/Questions/Questions.js
+++ client/src/components/Quiz/Questions/Questions.js
@@ -1,9 +1,9 @@
-import { useEffect, useState, useContext } from 'react';
+import { useEffect, useState } from 'react';
 
-import apiQuestionServices from '../../../sevices/api/apiQuestionServices';
+// import apiQuestionServices from '../../../sevices/api/apiQuestionServices';
 import apiResultServices from '../../../sevices/api/apiResultServices';
 // import triviaServices from '../../../sevices/trivia/triviaServices';
-import AppContext from '../../AppContext';
+// import AppContext from '../../AppContext';
 import { htmlDecode } from '../../../sevices/trivia/htmlHelper';
 
 import Qlist from '../Qlist';
@@ -11,30 +11,45 @@ import BasicTable from '../Resulttable';
 
 import './Questions.css'
 
-const Questions = ({ props, category, quizName }) => {
+const Questions = ({ props, quizName, questions, inLocal }) => {
 
-    const appContext = useContext(AppContext);
-    const url = props.location.pathname;
-    const inLocal = !url.includes('external');
-    let [catQuestions, setCatQuestions] = useState([]);
+    
+    let q = null;
+
+    // const appContext = useContext(AppContext);
+    // const url = props.location.pathname;
+    // const inLocal = !url.includes('external');
+    // let [catQuestions, setCatQuestions] = useState([]);
+    let [currentQuestion, setCurrentQuestion] = useState(0);
     let [score, setScore] = useState(0);
     let [userAnswers, setUserAnswers] = useState([]);
-    category = category.toLowerCase();
+    let [endOfQuiz, setEndOfQuiz] = useState(false);
 
     useEffect(() => {
-        if (inLocal) {
-            apiQuestionServices.getCategory(category)
-                .then(questions => {
-                    setCatQuestions(questions);
-                })
-                .catch(err => console.log(err));
-        } else {
-            setCatQuestions(appContext.trivia);
+        console.log('userAnswers');
+        if (endOfQuiz) {
+            console.log('End');
+            console.log(userAnswers);
+            const userToUpdate = JSON.parse(localStorage.getItem('sid')).user._id;
+            console.log(userAnswers)
+            apiResultServices.add({ userToUpdate, quizName, userAnswers });
+            // setEndOfQuiz(true);
+            // questions.length = 0;
+            // return (
+            // <div>
+            //     {/* <h1>Your score is: {score}</h1> */}
+            //     <div className="quiz-results">
+            //         <BasicTable rows={userAnswers} score={score} quizName={quizName} />
+            //         {inLocal ? <h1 className="reload" onClick={() => window.location.reload()}>Try again?</h1> : ''}
+            //     </div>
+            // </div>
+            // )
         }
-    }, [category, url, appContext.trivia, inLocal]);
+    }, [userAnswers, questions, quizName, userAnswers, inLocal, score]);
 
     const handleItemClick = (event, question, correctAnswer, selected, id) => {
         event.preventDefault();
+        console.log(currentQuestion);
         const isCorrect = selected === correctAnswer;
         const status = isCorrect ? 'correct' : 'wrong';
         const currResult = { question, selected, correctAnswer, isCorrect, status, id };
@@ -44,43 +59,69 @@ const Questions = ({ props, category, quizName }) => {
             setScore((oldState) => oldState + 1);
         }
         setUserAnswers(oldResult => oldResult.concat(currResult));
-        setCatQuestions((oldQuestions) => oldQuestions.slice(1));
+        // setCatQuestions((oldQuestions) => oldQuestions.slice(1));
+        if (currentQuestion !== questions.length - 1) {
+            setCurrentQuestion((previous => previous + 1));
+        } else {
+            setEndOfQuiz(true);
+            questions.length = 0;
+        }
     }
-    // console.log(score);
+    console.log(score);
     // console.log(catQuestions);
 
-    if (catQuestions.length === 0) {
-        
-        console.log(userAnswers);
-        const userToUpdate = JSON.parse(localStorage.getItem('sid')).user._id;
-        apiResultServices.add({userToUpdate, quizName, userAnswers});
-        return (
-            <div>
-                {/* <h1>Your score is: {score}</h1> */}
-                <div className="quiz-results">
-                    <BasicTable rows={userAnswers} score={score} quizName={quizName} />
-                    {inLocal ? <h1 className="reload" onClick={() => window.location.reload()}>Try again?</h1> : ''}
-                </div>
-            </div>
-        )
+    // if (catQuestions.length === 0) {
+
+    //     console.log(userAnswers);
+    //     const userToUpdate = JSON.parse(localStorage.getItem('sid')).user._id;
+    //     apiResultServices.add({userToUpdate, quizName, userAnswers});
+    //     return (
+    //         <div>
+    //             {/* <h1>Your score is: {score}</h1> */}
+    //             <div className="quiz-results">
+    //                 <BasicTable rows={userAnswers} score={score} quizName={quizName} />
+    //                 {inLocal ? <h1 className="reload" onClick={() => window.location.reload()}>Try again?</h1> : ''}
+    //             </div>
+    //         </div>
+    //     )
+    // }
+
+    if (!endOfQuiz && (questions.length === 0)) {
+        return null;
+    }   
+
+    console.log(questions);
+    console.log('Questions render');
+    if (questions.length !== 0) {
+        q = questions[currentQuestion];
+        q.question = htmlDecode(q.question);
+        q.answer = htmlDecode(q.answer);
+        q.incorrect_answers = q.incorrect_answers.map(htmlDecode);
     }
-    const q = catQuestions[0];
-    q.question = htmlDecode(q.question);
-    q.answer = htmlDecode(q.answer);
-    q.incorrect_answers = q.incorrect_answers.map(htmlDecode);
 
     return (
         <div className="questions">
             <ul className="question-list">
                 {/* {catQuestions.map(q => ( */}
-                <Qlist
-                    key={q._id}
-                    id={q._id}
-                    question={q.question}
-                    incAnswers={q.incorrect_answers}
-                    answer={q.correct_answer}
-                    onClick={handleItemClick}
-                />
+                {q && 
+                    <Qlist
+                        key={q._id}
+                        id={q._id}
+                        question={q.question}
+                        incAnswers={q.incorrect_answers}
+                        answer={q.correct_answer}
+                        onClick={handleItemClick}
+                    />
+                }
+                {endOfQuiz &&
+                    <div>
+                        {/* <h1>Your score is: {score}</h1> */}
+                        <div className="quiz-results">
+                            <BasicTable rows={userAnswers} score={score} quizName={quizName} />
+                            {inLocal ? <h1 className="reload" onClick={() => window.location.reload()}>Try again?</h1> : ''}
+                        </div>
+                    </div>
+                }
                 {/*))}*/}
             </ul>
         </div>
--- client/src/components/Quiz/Quiz/Quiz.js
+++ client/src/components/Quiz/Quiz/Quiz.js
@@ -1,13 +1,42 @@
+import { useEffect, useState, useContext } from 'react';
+
+import AppContext from '../../AppContext';
+import apiQuestionServices from '../../../sevices/api/apiQuestionServices';
+
 import Quizheader from '../Quizheader';
 import Questions from '../Questions';
 
+
 const Quiz = (props) => {
+    const appContext = useContext(AppContext);
+
     const quizName = props.match.params.quizName.toUpperCase();
     const category = props.match.params.category;
+    const url = props.location.pathname;
+    const inLocal = !url.includes('external');
+
+
+    let [questions, setQuestions] = useState([]);
+
+console.log('render Quiz component');
+    useEffect(() => {
+        console.log('in useEffect of Quiz');
+        if (inLocal) {
+            apiQuestionServices.getCategory(category)
+                .then(apiQuestions => {
+                    console.log(apiQuestions);
+                    setQuestions(apiQuestions);
+                })
+                .catch(err => console.log("Quiz component Error:" + err));
+        } else {
+            setQuestions(appContext.trivia);
+        }
+    }, [category, url, appContext.trivia, inLocal])
+
     return (
         <div className="quiz-content">
             <Quizheader quizName={quizName} />
-            <Questions category={category} props={props} quizName={quizName} />
+            {questions && <Questions props={props} quizName={quizName} questions={questions} inLocal={inLocal}/>}
         </div>
     );
 }
