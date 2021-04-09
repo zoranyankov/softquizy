a26c202214f4c4cfc104baecba21a1fb12e1e3bb
zoranyankov <zoran_yankov@yahoo.com>
2021-04-09 23:47:20 +0300

    update server for deploy

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const routes = require('./routes');
@@ -11,7 +12,9 @@ require('./config/express')(app);
 require('./config/mongoose.js')();
 
 app.use(cors());
+// app.use(express.static(path.join(__dirname, "./build")));
 app.use(routes);
+// app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./build/index.html")));
 app.use(errorHandler());
 
 app.listen(config.PORT, () => console.log(`Server is listening on http://localhost:${config.PORT}/...`));
